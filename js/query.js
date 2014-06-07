733d64d77827454351ca40c408644aa8135c312a
VaradPathak <pathak.varad@gmail.com>
2014-06-07 18:36:29 -0400

    Sequence of points

--- js/query.js
+++ js/query.js
@@ -36,6 +36,31 @@ function timlineQuery() {
 					"optional{?placemark geo-vis:hasPopup ?popup."+
 						"?popup geo-vis:popup-comment ?comment.}}";
 
+	var lineSeqQuery = "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
+						"prefix gml: <http://www.opengis.net/gml/> "+
+						"prefix geo2003: <http://www.w3.org/2003/01/geo/wgs84_pos#> "+
+						"prefix geo-vis: <http://www.isi.edu/karma/visualization/> "+
+						"prefix owl: <http://www.w3.org/2002/07/owl#> "+
+
+						"SELECT DISTINCT ?LineTitle (concat(concat(?lat,\" \"),?long) AS ?positions) ?seq ?startTime ?endTime ?weight ?color ?comment "+
+						"WHERE {?line rdf:type gml:LineString; "+
+								"geo-vis:title ?LineTitle. "+
+								"{ "+
+									"SELECT ?lat ?long ?startTime ?endTime ?seq "+
+									"WHERE{?line geo-vis:vertex ?vertex. "+
+											"?vertex geo-vis:start-time ?startTime; "+
+											"geo-vis:end-time ?endTime; "+
+											"geo2003:lat ?lat; "+
+											"geo2003:long ?long; "+
+											"geo-vis:sequence ?seq. "+
+									"}order by ?seq "+
+								"} "+
+								"optional{?line geo-vis:lineWidth ?weight} "+
+								"optional{?line geo-vis:hasColor ?color} "+
+								"optional{?line geo-vis:hasPopup ?popup. "+
+								"?popup geo-vis:popup-comment ?comment.} "+
+						"}";
+
 	var polygonQuery = "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
 				"prefix gml: <http://www.opengis.net/gml/> "+
 				"prefix geo2003: <http://www.w3.org/2003/01/geo/wgs84_pos#>"+
@@ -55,7 +80,7 @@ function timlineQuery() {
 
 	var baseURL = "http://localhost:8080/openrdf-sesame/repositories/karma_data";
 
-	sparqlQuery(polygonQuery, baseURL);
+	sparqlQuery(lineSeqQuery, baseURL);
 }
 function display(binding) {
 	var result = "[";
@@ -182,6 +207,52 @@ function polygonDisplay(binding) {
 	result = result + "]";
 	return result;
 }
+
+function polyLineDisplay(binding) {
+	
+	var result = "[";
+	for (var i = 1; i < binding.length; i++) {
+		result = result + "{\"start\" : \"" + binding[i].startTime.value + "\",";
+		result = result + "\"end\" : \"" + binding[i].endTime.value+"\",";
+		result = result + "\"polyline\" : [";
+		var latlong = binding[i-1].positions.value;
+		latlong = latlong.split(" ");
+		var lat = latlong[0];
+		var lon = latlong[1];
+		result = result + "{\"lat\" : " + lat + ", \"lon\" : " + lon + "},";
+		
+		latlong = binding[i].positions.value;
+		latlong = latlong.split(" ");
+		lat = latlong[0];
+		lon = latlong[1];
+		result = result + "{\"lat\" : " + lat + ", \"lon\" : " + lon + "}],";
+
+		result = result + "\"title\" : \"" + binding[i].LineTitle.value + "\",";
+		result = result + "\"options\" : { ";
+		if (binding[i].color) {
+			result = result + "\"theme\" : \"" + binding[i].color.value + "\",";
+		};
+
+		if (binding[i].comment) {
+			result = result + "\"description\" : \"" + binding[i].comment.value + "\",";
+		};
+
+		if (binding[i].weight) {
+			result = result + "\"lineWeight\" : \"" + binding[i].weight.value;
+		};
+
+		result = result + "\"}";
+		if (i<binding.length-1) {
+			result = result + "},";
+		}else{
+			result = result + "}";
+		}
+	}
+	result = result + "]";
+	console.log(result);
+	return result;
+}
+
 function sparqlQuery(query, baseURL, format) {
 	if (!format)
 		format = "application/sparql-results+json";
@@ -216,8 +287,9 @@ function sparqlQuery(query, baseURL, format) {
 			var results = JSON.parse(xhr.responseText);
 			var binding = results.results.bindings;
 			// dataPoints = display(binding);
-			//dataPoints = lineDisplay(binding);
-			dataPoints = polygonDisplay(binding);
+			// dataPoints = lineDisplay(binding);
+			// dataPoints = polygonDisplay(binding);
+			dataPoints = polyLineDisplay(binding);
 			initTimemap(dataPoints);
 		}
 	};
@@ -249,7 +321,7 @@ function createCORSRequest(method, url) {
 
 
 function initTimemap(dataPoints) {
-	console.log(dataPoints);
+	// alert(dataPoints);
 	SimileAjax.History.enabled = false;
 	var tm = TimeMap.init({
         mapId: "map",               // Id of map div element (required)
